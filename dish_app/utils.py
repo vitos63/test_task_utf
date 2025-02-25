9a411610157b36ceaba373dda9b5d2422be96e1c
vitya.bashkov@mail.ru <vitya.bashkov@mail.ru>
2025-02-25 11:16:45 +0500

    refactor: improve function name for better understanding

--- dish_app/utils.py
+++ dish_app/utils.py
@@ -2,7 +2,7 @@ from django.db.models import Prefetch, QuerySet
 from dish_app.models import Food, FoodCategory
 
 
-def get_categories_with_published_food() -> QuerySet[FoodCategory]:
+def get_categories_with_published_foods() -> QuerySet[FoodCategory]:
     published_food = Food.objects.filter(is_publish=True)
     return (FoodCategory.objects.filter(food__is_publish=True).distinct()
         .prefetch_related(Prefetch('food', queryset=published_food))
--- dish_app/views.py
+++ dish_app/views.py
@@ -1,8 +1,8 @@
 from rest_framework.generics import ListAPIView
-from dish_app.utils import get_categories_with_published_food
+from dish_app.utils import get_categories_with_published_foods
 from dish_app.models import FoodListSerializer
 
 
 class FoodsAPIView(ListAPIView):
-    queryset = get_categories_with_published_food()
+    queryset = get_categories_with_published_foods()
     serializer_class = FoodListSerializer
